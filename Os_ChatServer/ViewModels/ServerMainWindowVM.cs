165973c48fdac4f3c406e53f89a89f5115b6cfb0
Sova IS <Sowa.igor2012@yandex.ru>
2021-04-02 22:32:27 +0500

    feat: added PortProvider.cs

--- Os_ChatServer/ViewModels/ServerMainWindowVM.cs
+++ Os_ChatServer/ViewModels/ServerMainWindowVM.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Concurrent;
 using System.Net.Sockets;
 
-
-
 namespace Os_ChatServer.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
---
+++ Utils/PortProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace Utils
+{
+    public static class PortProvider
+    {
+        public static int GetAvailablePort()
+        {
+            const int minPort=49152; 
+            const int maxPort=65535;
+
+            var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+            var busyPort = tcpConnInfoArray.Select(conn => conn.LocalEndPoint.Port).ToArray();
+            
+            for (var port = minPort; port <= maxPort; port++)
+            {
+                if (!busyPort.Contains(port))
+                {
+                    return port;
+                }
+            }
+
+            throw new ArgumentException("Can't find available Port for Client");
+        }
+
+    }
+}

87828cd082c217cde8d3f9b0ad8e313002630cd0
Sova IS <Sowa.igor2012@yandex.ru>
2021-04-10 23:03:03 +0500

    feat: added running two clients

---
+++ OS_ChatClient/Services/ServerMessagesListener.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Utils;
+
+namespace OS_ChatLabAvalonia.NETCoreMVVMApp.Services
+{
+    public class ServerMessagesListener
+    {
+        public const string BaseIpAddress = "127.0.0.";
+        private int _port;
+        private string _ipAddress;
+        
+        public ServerMessagesListener()
+        {
+            _port = PortProvider.GetAvailablePort();
+            _ipAddress = GetAvailableIp();
+            ThreadPool.QueueUserWorkItem(StartListenForServerMessages);
+        }
+
+        private string GetAvailableIp()
+        {
+            string fullAddress = string.Empty;
+            for (int i = 5; i < 255; i++)
+            {
+                fullAddress = BaseIpAddress + i;
+                try
+                {
+                    var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                    EndPoint endPoint = new IPEndPoint(IPAddress.Parse(fullAddress), _port);
+                    tcpClient.Bind(endPoint);
+                    return fullAddress;
+                }
+                catch (Exception e)
+                {
+                    continue;
+                }
+            }
+
+            throw new ArgumentOutOfRangeException();
+        }
+        
+        private void StartListenForServerMessages(object? state)
+        {
+            
+        }
+    }
+}
--- OS_ChatClient/ViewModels/ClientMainWindowVM.cs
+++ OS_ChatClient/ViewModels/ClientMainWindowVM.cs
@@ -1,15 +1,32 @@
 ﻿
 
 using System.Net;
+using System.Reactive;
+using OS_ChatLabAvalonia.NETCoreMVVMApp.Services;
+using ReactiveUI;
 
 namespace OS_ChatLabAvalonia.NETCoreMVVMApp.ViewModels
 {
     public sealed class MainWindowViewModel : ViewModelBase
     {
+        private readonly string _userName;
+        private readonly string _userToken;
+        private readonly IPEndPoint _serverEndPoint;
+        private readonly ServerMessagesListener _serverMessagesListener;
+        public ReactiveCommand<Unit, Unit> SendTextMessageCommand { get; }
+        public string ChatMessageText { get; set; }
         public MainWindowViewModel(string userName,string userToken,IPEndPoint serverEndPoint)
         {
-          
+            _userName = userName;
+            _userToken = userToken;
+            _serverEndPoint = serverEndPoint;
+            _serverMessagesListener = new ServerMessagesListener();
+            SendTextMessageCommand = ReactiveCommand.Create(SendTextMessage);
         }
-        
+
+        private void SendTextMessage()
+        {
+            
+        }
     }
 }
